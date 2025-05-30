bd4e7fe8c94d4f90045361cfef45617e6115f7d7
alicezinha123 <informaticaafron@gmail.com>
2025-05-29 21:59:50 -0300

    index.html commit

---
+++ script.js
@@ -0,0 +1,31 @@
+
+function startImageRotation(container) {
+    const images = container.querySelectorAll("img");
+    let index = 0;
+
+    images.forEach(img => img.classList.remove("active"));
+    images[0].classList.add("active");
+
+    container._currentIndex = 0;
+
+    if (container._intervalld) return;
+
+    container._intervalld = setInterval(() => {
+        images[container._currentIndex].classList.remove("active");
+        container._currentIndex = (container._currentIndex + 1) % images.length;
+        images[container._currentIndex].classList.add("active");
+    }, 1000);
+}
+
+function stopImageRotation(container) {
+    const images = container.querySelectorAll("img");
+
+    clearInterval(container._intervalld);
+    container._intervalld = null;
+
+    images.forEach(img => img.classList.remove("active"));
+    if (images.length > 0) {
+        images[0].classList.add("active");
+        container._currentIndex = 0;
+    }
+}
